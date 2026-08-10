2aab2635ead0cd11486ecb59ecb05678092554b0
JJOverload <jay.szeto.23@gmail.com>
2026-08-10 16:39:51 -0700

    Add and Update files. Letting simple script to try its process for multiple images. Gets results much faster than tesseract for real life photos

--- easyocr-version/easy-test1.py
+++ easyocr-version/easy-test1.py
@@ -1,6 +1,6 @@
 import easyocr
 
-reader = easyocr.Reader(["en"])
+reader = easyocr.Reader(["en"], gpu=False)
 
 #results = reader.readtext("card.jpg")
 results = reader.readtext("1_python-ocr.jpg")
@@ -8,3 +8,32 @@ results = reader.readtext("1_python-ocr.jpg")
 for bbox, text, confidence in results:
     print(text)
     print(confidence)
+
+print("----------------FyndhornElves1.jpg------------------------")
+
+results = reader.readtext("FyndhornElves1.jpg")
+
+for bbox, text, confidence in results:
+    print(text)
+    print(confidence)
+
+
+
+print("----------------CardPileSample1.jpg------------------------")
+
+results = reader.readtext("CardPileSample1.jpg")
+
+for bbox, text, confidence in results:
+    print(bbox)
+    print(text)
+    print(confidence)
+
+
+print("----------------tegwyll-nonlands-Copy.jpg------------------------")
+
+results = reader.readtext("tegwyll-nonlands-Copy.jpg")
+
+for bbox, text, confidence in results:
+    print(text)
+    print(confidence)
+
