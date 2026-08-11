8771a505dc99fc9cc6843d201c26b6957b08694a
JJOverload <jay.szeto.23@gmail.com>
2026-08-10 19:19:04 -0700

    Add easy-test1.py - Refractored code and added variation that includes cv2 (OpenCV) module library

--- easyocr-version/easy-test1.py
+++ easyocr-version/easy-test1.py
@@ -1,39 +1,17 @@
+import cv2 as cv
 import easyocr
 
 reader = easyocr.Reader(["en"], gpu=False)
 
 #results = reader.readtext("card.jpg")
-results = reader.readtext("1_python-ocr.jpg")
 
-for bbox, text, confidence in results:
-    print(text)
-    print(confidence)
+scan_names = ["1_python-ocr.jpg", "FyndhornElves1.jpg", "CardPileSample1.jpg", "tegwyll-nonlands-Copy.jpg"]
 
-print("----------------FyndhornElves1.jpg------------------------")
-
-results = reader.readtext("FyndhornElves1.jpg")
-
-for bbox, text, confidence in results:
-    print(text)
-    print(confidence)
-
-
-
-print("----------------CardPileSample1.jpg------------------------")
-
-results = reader.readtext("CardPileSample1.jpg")
-
-for bbox, text, confidence in results:
-    print(bbox)
-    print(text)
-    print(confidence)
-
-
-print("----------------tegwyll-nonlands-Copy.jpg------------------------")
-
-results = reader.readtext("tegwyll-nonlands-Copy.jpg")
-
-for bbox, text, confidence in results:
-    print(text)
-    print(confidence)
-
+for name in scan_names:
+    image = cv.imread(name)
+    results = reader.readtext(image)
+    for bbox, text, confidence in results:
+        print("Text:", text)
+        print("Confidence:", confidence)
+        print("Bounding Boxes:", bbox)
+        print("--------------------------------------------")
